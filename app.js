c0d79076340bb9f2bc6f7f83fdf572317542acaa
sturpin <turpiniko@gmail.com>
2021-12-24 00:23:53 +0100

    Handlebars first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const hbs = require('hbs');
+
+const app = express()
+app.listen(3000)
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estático
+app.use( express.static('public') );
+
+
+// Rutas
+app.get('/', (req, res) => {
+  res.render('home', {
+    titulo: 'Handlebars Engine',
+    nombre: 'by Sergio Turpín'
+  });
+})
+
+app.get('/generic', (req, res) => {
+  res.render('generic', {
+    titulo: 'Handlebars Engine',
+    nombre: 'by Sergio Turpín'
+  });
+})
+
+app.get('/elements', (req, res) => {
+  res.render('elements', {
+    titulo: 'Handlebars Engine',
+    nombre: 'by Sergio Turpín'
+  });
+})
+
+app.get('*', (req, res) => {
+  res.sendFile( __dirname + '/public/back/404.html')
+})
